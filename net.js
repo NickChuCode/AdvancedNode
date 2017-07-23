eac8ef5ab99dfdea90580d02af423e0579887520
NickChuCode <nickchu15@outlook.com>
2017-07-23 16:54:12 +0800

    add chat among clients

--- net.js
+++ net.js
@@ -1,22 +1,27 @@
 process.stdout.write('\u001B[2J\u001B[0:0f');
 let counter = 0;
+let sockets = {};
 
 const server = require('net').createServer();
 
 server.on('connection', socket => {
     //本node文件通过nc来触发connection,打开server的connection
     socket.id = counter++;
+    sockets[socket.id] = socket;
     console.log('client connected');
     socket.write('welcome new client!\n');
 
     socket.on('data', data => {
         //如果没有指定encode，data会以buffer的形式输出
-        console.log('data is: ', data);
-        socket.write(`${socket.id}: `);
-        socket.write(data, 'utf8');//这里默认是utf8，可以不设
+        Object.entries(sockets).forEach(([, cs]) => {
+            cs.write(`${socket.id}: `);
+            cs.write(data);
+        });
     });
 
     socket.on('end', () => {
+        //当一个client退出时，从client的列表中删除
+        delete sockets[socket.id];
         //这里通过在nc中CTRL+D来关闭session
         console.log('client disconnected!');
     });
